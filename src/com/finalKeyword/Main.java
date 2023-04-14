78f865413485bfce9e552020b641d0433082391c
mihir <mihirparmar031@gmail.com>
2023-04-14 16:35:12 +0530

    initial commit

---
+++ src/com/finalKeyword/Main.java
@@ -0,0 +1,15 @@
+package com.finalKeyword;
+import com.finalKeyword.time.*;
+public class Main extends Time {
+//@Override
+//   public void time(){
+//     System.out.println("there are 1440 minutes in a day");
+//   } // error: time()' cannot override 'time()' in 'com.finalKeyword.time.Time'; overridden method is final
+
+  public static void main(String[] args) {
+
+    Time time=new Time();
+//    time.hour=45;  // error:Cannot assign a value to final variable 'hour'
+     time.time();
+  }
+}
